32635a8bba21ddc10259a23889077816ac2598ae
Jennifer Xiong <jenni4xiong@gmail.com>
2020-05-11 23:16:16 -0700

    final commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,9 +3,26 @@
 //   return document.getElementsByClassName(className);
 // };
 
+//ask about element.classList??
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var pushElementsThatContainTheClass = function(className, element, elementsThatContainTheClass) {
+  if(element.classList && element.classList.contains(className)) {
+    elementsThatContainTheClass.push(element);
+  }
+  //iterate through the child nodes
+  const childNodes = element.childNodes;
+  for (var i = 0; i < element.childNodes.length; i++) {
+    const child = childNodes[i];
+    pushElementsThatContainTheClass(className, child, elementsThatContainTheClass);
+  }
+}
 
+var getElementsByClassName = function(className) {
+  // your code here
+  //You should use document.body, element.childNod
+  const result = [];
+    //if it is, then we want to add to the array.
+    pushElementsThatContainTheClass(className, document.body, result);
+  return result;
 };
+
